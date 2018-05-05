52ac2702e8cab5dc75008b85a6317dc8b03a16b1
Brian Farnsworth <farnswor@adobe.com>
2018-05-05 16:15:48 -0600

    wip

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => {
-   console.log(`Listening on ${ PORT }`
-
-console.log('process.env.EVERNOTE_API_CONSUMER_SECRET: ', process.env.EVERNOTE_API_CONSUMER_SECRET);
-      
-      
-}))
+   console.log(`Listening on ${ PORT }`);
+   console.log('process.env.EVERNOTE_API_CONSUMER_SECRET: ', process.env.EVERNOTE_API_CONSUMER_SECRET);
+   })
